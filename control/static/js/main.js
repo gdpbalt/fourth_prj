0e33947e90ae9df81790508e4282c0f4c95f7669
Dmitry <gdp@odev.io>
2021-06-04 17:56:22 +0300

    add java script code for form date_stop for tour

--- control/static/js/main.js
+++ control/static/js/main.js
@@ -5,12 +5,33 @@ $(function() {
         minLength: 0
     })
 
-    $('#destination_all').click(function(e) {
+
+    $('#destination_all').click(function() {
         $('#destination').autocomplete("search");
     });
 
+    function set_date_stop() {
+        let date = new Date($('#tour_date_start').val());
+        let newdate = new Date(date);
+
+        newdate.setDate(newdate.getDate() + 14);
+
+        let dd = newdate.getDate();
+        let mm = newdate.getMonth() + 1;
+        let yy = newdate.getFullYear();
+
+        let someFormattedDate = yy + '-' + ('0' + mm).slice(-2) + '-' + ('0' + dd).slice(-2);
+        $('#tour_date_stop').val(someFormattedDate)
+    }
+
+    $('#tour_date_start').change(function() {
+        set_date_stop()
+    });
+
+
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
+        set_date_stop()
     })
 
 });
