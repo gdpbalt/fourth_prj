f8cddf8a7e68459f9840908b5bdff97882702f75
Dmitry <gdp@odev.io>
2021-06-20 11:02:45 +0300

    refactoring

--- control_service.py
+++ control_service.py
@@ -2,6 +2,7 @@
 import argparse
 import datetime
 from time import sleep
+from typing import Any
 
 from sqlalchemy import exc
 
@@ -15,78 +16,88 @@ from control.settings import SEARCH_UPDATE_MINUTES, SEARCH_TRY_AFTER_ERROR_HOURS
 datetime_format = '%Y-%m-%d %H:%M:%S'
 
 
-def run_search(force_flag: bool = False):
-    """
-        Выбрать туры, которые требуется обновить
-        В первую очередь запросить информацию по новым турам
-    :return: None
-    """
+def get_run_search_sql(force_flag: bool, date_search: datetime.datetime,
+                       date_search_after_error: datetime.datetime) -> list:
     lang_id = LANGS.index('rus')
-    start_time = datetime.datetime.now()
-    date_search = start_time - datetime.timedelta(minutes=SEARCH_UPDATE_MINUTES)
-    date_search_after_error = start_time - datetime.timedelta(hours=SEARCH_TRY_AFTER_ERROR_HOURS)
 
     sql = db.session.query(Tour.showcase_id, Tour.id, TourSearch.updated, Tour.link)
     sql = sql.outerjoin(TourSearch, Tour.id == TourSearch.tour_id)
     sql = sql.filter(Tour.active == True)
     sql = sql.filter(db.or_(TourSearch.lang == None, TourSearch.lang == lang_id))
-
     if force_flag is False:
         sql = sql.filter(db.or_(Tour.errors < SEARCH_STOP_AFTER_ERRORS, Tour.errors_update <= date_search_after_error))
-
     if force_flag is False:
         sql = sql.filter(db.or_(TourSearch.updated == None, TourSearch.updated <= date_search))
     sql = sql.order_by(TourSearch.updated)
-    sql = sql.all()
+    return sql.all()
+
+
+def do_run_search(tour_id: int, url_link: str) -> None:
+    try:
+        search_request = MethodSearch(index=tour_id, url_link=url_link)
+        search_request.run()
+    except Exception as e:
+        msg = f'Some error occurred. {e}'
+        app.logger.error(msg)
+
+
+def get_delay_of_runnig(start_time: datetime) -> int:
+    stop_time = datetime.datetime.now()
+    delay = stop_time - start_time
+    return int(delay.total_seconds())
+
+
+def get_run_correct_sql(date_now: datetime.date) -> list[Tour]:
+    sql = db.session.query(Tour)
+    sql = sql.filter(Tour.active == True)
+    sql = sql.filter(Tour.date_start <= date_now)
+    return sql.all()
+
+
+def start_search(force_flag: bool = False):
+    """
+        Обновить данные по турам, которые обновлялись некоторое время тому назад
+        В первую очередь запросить информацию по новым турам, по которым нет данных
+
+    :return: None
+    """
+    number, date_now = 0, datetime.datetime.now()
+    date_search = date_now - datetime.timedelta(minutes=SEARCH_UPDATE_MINUTES)
+    date_search_after_error = date_now - datetime.timedelta(hours=SEARCH_TRY_AFTER_ERROR_HOURS)
+    sql = get_run_search_sql(force_flag=force_flag, date_search=date_search,
+                             date_search_after_error=date_search_after_error)
 
     app.logger.info('*** Start searching at {}. Total {} records'.format(
-        start_time.strftime(datetime_format), len(sql)))
+        date_now.strftime(datetime_format), len(sql)))
     app.logger.info('*** Search tours where last updated less than {} and last error less than {}'.format(
         date_search.strftime(datetime_format), date_search_after_error))
 
-    number = 0
     for showcase, tour, update, link in sql:
-        if number > 0:
-            app.logger.info(f'*** Sleep {SEARCH_PAUSE_BETWEEN_REQUEST_SECOND} seconds between requests to Otpusk')
+        if (number := number + 1) > 1:
+            app.logger.info(f'*** Sleep {SEARCH_PAUSE_BETWEEN_REQUEST_SECOND} seconds between Search')
             sleep(SEARCH_PAUSE_BETWEEN_REQUEST_SECOND)
-        number += 1
-        app.logger.info("*** Record={}, showcase={}, tour={}, last updated={}".
-                        format(number, showcase, tour,
-                               update.strftime(datetime_format) if update is not None else 'Null'))
 
-        try:
-            search_request = MethodSearch(index=tour, url_link=link)
-            search_request.run()
-        except Exception as e:
-            msg = f'Some error occurred. {e}'
-            app.logger.error(msg)
+        app.logger.info("*** Record={}, showcase={}, tour={}, last updated={}".format(
+            number, showcase, tour, update.strftime(datetime_format) if update is not None else 'Null'))
 
-        if force_flag is True:
-            stop_time = datetime.datetime.now()
-            delay = stop_time - start_time
-            if delay.total_seconds() > SEARCH_STOP_AFTER_SECOND:
-                break
+        do_run_search(tour_id=tour, url_link=link)
+        if force_flag is False and get_delay_of_runnig(start_time=date_now) > SEARCH_STOP_AFTER_SECOND:
+            break
 
-    stop_time = datetime.datetime.now()
-    delay = stop_time - start_time
-    app.logger.info('*** Stop searching at {}. Proccessing {} records in {:.2f} minutes ***'.
-                    format(stop_time.strftime(datetime_format), number, delay.total_seconds() / 60))
+    app.logger.info('*** Stop searching. Proccessing {} records in {:.2f} minutes ***'.format(
+        number, get_delay_of_runnig(start_time=date_now) / 60))
 
 
-def run_correct():
+def start_correct_date_start_stop():
     """
-    Выбрать туры, дата вылета в которые старше чем завтра и обновить их date_start и date_stop
+    Выбрать туры, дата вылета в которые меньше чем завтра. Обновить такие туры (date_start и date_stop)
 
     :return: None
     """
     date_now = datetime.datetime.now().date()
     date_start = date_now + datetime.timedelta(days=1)
     date_stop = date_start + datetime.timedelta(days=SEARCH_INTERVAL_DAYS)
-
-    sql = db.session.query(Tour)
-    sql = sql.filter(Tour.active == True)
-    sql = sql.filter(Tour.date_start <= date_now)
-    sql = sql.all()
+    sql = get_run_correct_sql(date_now=date_now)
 
     app.logger.debug(f'*** Сегодня: {date_now}, Завтра: {date_start}, Интервал: {SEARCH_INTERVAL_DAYS} дней, '
                      f'Конечная дата: {date_stop}, Найдено: {len(sql)} туров')
@@ -97,12 +108,15 @@ def run_correct():
         tour.date_start = date_start
         tour.date_stop = date_stop
 
-        # TODO проанализировать возможную ошибку, как в методе search
-        data_link = MakeSearchLink(index=tour.id)
-        data_link.run()
-        tour.link = data_link.link
+        try:
+            data_link = MakeSearchLink(index=tour.id)
+            data_link.run()
+            tour.link = data_link.link
+        except Exception as e:
+            msg = f'Some error occurred. {e}'
+            app.logger.error(msg)
+            break
 
-        app.logger.debug(f'НОВОЕ ЗНАЧЕНИЕ: start={tour.date_start}, stop={tour.date_stop}')
         try:
             db.session.commit()
             app.logger.info(f"Данные о туре id={tour.id} успешно обновлены")
@@ -111,28 +125,42 @@ def run_correct():
             app.logger.error(f"{msg}. {e}")
 
 
-def disable_failed_tours():
+def get_disable_failed_tours_sql(date_stop: datetime.date) -> list[Any]:
+    lang_id = LANGS.index('rus')
+    date_stop_dt = datetime.datetime.combine(date=date_stop, time=datetime.time(hour=0, minute=0, second=0))
+
+    sql = db.session.query(Tour, TourSearch.updated)
+    sql = sql.outerjoin(TourSearch, Tour.id == TourSearch.tour_id)
+    sql = sql.filter(Tour.active == True)
+    sql = sql.filter(db.or_(TourSearch.lang == None, TourSearch.lang == lang_id))
+    sql = sql.filter(db.or_(TourSearch.updated <= date_stop_dt, Tour.errors > SEARCH_DISABLE_AFTER_ERRORS))
+    sql = sql.order_by(TourSearch.updated)
+    return sql.all()
+
+
+def start_disable_failed_tours():
     """
+    Выбрать все туры, которые имеют кол-во ошибок больше заданного уровня значения
+    или обновлялись последний раз раньше чем заданный день
+
+    Запретить такие туры к обработке (установить Tour.active = False)
 
     :return: None
     """
-    date_now = datetime.datetime.now()
-    date_stop_try = date_now - datetime.timedelta(days=SEARCH_DISABLE_AFTER_FAILED_UPATE_DAYS)
-    app.logger.debug(f'*** Сегодня: {date_now}, '
-                     f'не обрабатывать туры старше {date_stop_try} ({SEARCH_DISABLE_AFTER_FAILED_UPATE_DAYS} дня) '
-                     f'или больше чем {SEARCH_DISABLE_AFTER_ERRORS} ошибок')
+    date_now = datetime.datetime.now().date()
+    date_stop_trying = date_now - datetime.timedelta(days=SEARCH_DISABLE_AFTER_FAILED_UPATE_DAYS)
+    app.logger.debug('*** Сегодня: {}, не обрабатывать туры старше {} ({} дня назад) или больше чем {} ошибок'.format(
+        date_now, date_stop_trying, SEARCH_DISABLE_AFTER_FAILED_UPATE_DAYS, SEARCH_DISABLE_AFTER_ERRORS))
 
-    sql = db.session.query(Tour.showcase_id, Tour.id, Tour.updated, Tour.errors, TourSearch.updated)
-    sql = sql.outerjoin(TourSearch, Tour.id == TourSearch.tour_id)
-    sql = sql.filter(Tour.active == True)
-    sql = sql.filter(db.or_(TourSearch.updated <= date_stop_try, Tour.errors > SEARCH_DISABLE_AFTER_ERRORS))
-    sql = sql.order_by(TourSearch.updated)
+    sql = get_disable_failed_tours_sql(date_stop=date_stop_trying)
+    for record in sql:
+        tour = record.Tour
+        error_updated = record.updated
+        print("Showcase={}, Tour={}, Tour.errors={}, TourSearch.updated={}".format(
+            tour.showcase_id, tour.id, tour.errors, error_updated))
 
-    print(sql)
-    sql = sql.all()
-
-    for tour in sql:
-        print("Showcase={}, Tour={}, Errors={}, Update={}".format(*tour))
+        tour.active = False
+        tour.updated = datetime.datetime.now()
 
 
 if __name__ == "__main__":
@@ -157,8 +185,8 @@ if __name__ == "__main__":
         is_force = False
 
     if args.run:
-        run_search(force_flag=is_force)
+        start_search(force_flag=is_force)
     elif args.correct:
-        run_correct()
+        start_correct_date_start_stop()
     elif args.error:
-        disable_failed_tours()
+        start_disable_failed_tours()
