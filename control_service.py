361cb304a912faf5642a77a7835592bb428e0648
Dmitry <gdp@odev.io>
2021-06-20 11:49:07 +0300

    refactoring

--- control_service.py
+++ control_service.py
@@ -54,6 +54,19 @@ def get_run_correct_sql(date_now: datetime.date) -> list[Tour]:
     return sql.all()
 
 
+def get_disable_failed_tours_sql(date_stop: datetime.date) -> list[Any]:
+    lang_id = LANGS.index('rus')
+    date_stop_dt = datetime.datetime.combine(date=date_stop, time=datetime.time(hour=0, minute=0, second=0))
+
+    sql = db.session.query(Tour, TourSearch.updated)
+    sql = sql.outerjoin(TourSearch, Tour.id == TourSearch.tour_id)
+    sql = sql.filter(Tour.active == True)
+    sql = sql.filter(db.or_(TourSearch.lang == None, TourSearch.lang == lang_id))
+    sql = sql.filter(db.or_(TourSearch.updated <= date_stop_dt, Tour.errors > SEARCH_DISABLE_AFTER_ERRORS))
+    sql = sql.order_by(TourSearch.updated)
+    return sql.all()
+
+
 def start_search(force_flag: bool = False):
     """
         Обновить данные по турам, которые обновлялись некоторое время тому назад
@@ -99,8 +112,8 @@ def start_correct_date_start_stop():
     date_stop = date_start + datetime.timedelta(days=SEARCH_INTERVAL_DAYS)
     sql = get_run_correct_sql(date_now=date_now)
 
-    app.logger.debug(f'*** Сегодня: {date_now}, Завтра: {date_start}, Интервал: {SEARCH_INTERVAL_DAYS} дней, '
-                     f'Конечная дата: {date_stop}, Найдено: {len(sql)} туров')
+    app.logger.info('*** Сегодня: {}, Завтра: {}, Интервал: {} дней, Конечная дата: {}, Найдено: {} туров'.format(
+        date_now, date_start, SEARCH_INTERVAL_DAYS, date_stop, len(sql)))
     for tour in sql:
         app.logger.debug(f'Витрина={tour.showcase_id}, Тур={tour.id}, Активный={tour.active}')
         app.logger.debug(f'СТАРОЕ ЗНАЧЕНИЕ: start={tour.date_start}, stop={tour.date_stop}')
@@ -119,23 +132,10 @@ def start_correct_date_start_stop():
 
         try:
             db.session.commit()
-            app.logger.info(f"Данные о туре id={tour.id} успешно обновлены")
+            app.logger.info(f"Данные о туре {tour.showcase_id}/{tour.id} успешно обновлены")
         except exc.SQLAlchemyError as e:
-            msg = f"При изменении тура id={tour.id} произошла ошибка"
+            msg = f"При изменении тура {tour.showcase_id}/{tour.id} произошла ошибка"
             app.logger.error(f"{msg}. {e}")
-
-
-def get_disable_failed_tours_sql(date_stop: datetime.date) -> list[Any]:
-    lang_id = LANGS.index('rus')
-    date_stop_dt = datetime.datetime.combine(date=date_stop, time=datetime.time(hour=0, minute=0, second=0))
-
-    sql = db.session.query(Tour, TourSearch.updated)
-    sql = sql.outerjoin(TourSearch, Tour.id == TourSearch.tour_id)
-    sql = sql.filter(Tour.active == True)
-    sql = sql.filter(db.or_(TourSearch.lang == None, TourSearch.lang == lang_id))
-    sql = sql.filter(db.or_(TourSearch.updated <= date_stop_dt, Tour.errors > SEARCH_DISABLE_AFTER_ERRORS))
-    sql = sql.order_by(TourSearch.updated)
-    return sql.all()
 
 
 def start_disable_failed_tours():
@@ -149,18 +149,24 @@ def start_disable_failed_tours():
     """
     date_now = datetime.datetime.now().date()
     date_stop_trying = date_now - datetime.timedelta(days=SEARCH_DISABLE_AFTER_FAILED_UPATE_DAYS)
-    app.logger.debug('*** Сегодня: {}, не обрабатывать туры старше {} ({} дня назад) или больше чем {} ошибок'.format(
+    app.logger.info('*** Сегодня: {}, не обрабатывать туры старше {} ({} дня назад) или больше чем {} ошибок'.format(
         date_now, date_stop_trying, SEARCH_DISABLE_AFTER_FAILED_UPATE_DAYS, SEARCH_DISABLE_AFTER_ERRORS))
 
     sql = get_disable_failed_tours_sql(date_stop=date_stop_trying)
     for record in sql:
         tour = record.Tour
         error_updated = record.updated
-        print("Showcase={}, Tour={}, Tour.errors={}, TourSearch.updated={}".format(
+        app.logger.info("Showcase={}, Tour={}, Tour.errors={}, TourSearch.updated={}".format(
             tour.showcase_id, tour.id, tour.errors, error_updated))
 
         tour.active = False
         tour.updated = datetime.datetime.now()
+        try:
+            db.session.commit()
+            app.logger.info(f"Данные о туре {tour.showcase_id}/{tour.id} успешно обновлены")
+        except exc.SQLAlchemyError as e:
+            msg = f"При изменении тура {tour.showcase_id}/{tour.id} произошла ошибка"
+            app.logger.error(f"{msg}. {e}")
 
 
 if __name__ == "__main__":
