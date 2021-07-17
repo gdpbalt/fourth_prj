8d53365a89f4ca76e07428ef51d13ba89cce2b98
root <gdp@odev.io>
2021-07-17 18:09:33 +0300

    Update scripts

--- srcipts/control_services_update.sh
+++ srcipts/control_services_update.sh
@@ -2,5 +2,19 @@
 
 cd /home/ubuntu/control/flask
 
-/home/ubuntu/control/flask/venv/bin/python3 /home/ubuntu/control/flask/sync_otpusk_local.py $1
+# --countries  Sync otpusk.countries with dbase
+# --from       Sync otpusk.from_cities with dbase
+# --operators  Sync otpusk.operators with dbase
+# --cities     Sync otpusk.cities with dbase
+# --ports      Sync otpusk.ports with dbase
+
+/home/ubuntu/control/flask/venv/bin/python3 /home/ubuntu/control/flask/sync_otpusk_and_local.py --countries
+
+/home/ubuntu/control/flask/venv/bin/python3 /home/ubuntu/control/flask/sync_otpusk_and_local.py --from
+
+/home/ubuntu/control/flask/venv/bin/python3 /home/ubuntu/control/flask/sync_otpusk_and_local.py --operators
+
+/home/ubuntu/control/flask/venv/bin/python3 /home/ubuntu/control/flask/sync_otpusk_and_local.py --cities
+
+/home/ubuntu/control/flask/venv/bin/python3 /home/ubuntu/control/flask/sync_otpusk_and_local.py --ports
 
--- srcipts/run-proccess.sh
+++ srcipts/run-proccess.sh
@@ -6,6 +6,11 @@ RUN=control_update.sh
 if ! /usr/bin/pgrep -al -f $RUN > /dev/null 2>/dev/null
 then
 	echo "Starting process"
+	date
+
 	/home/ubuntu/control/bin/$RUN
+
+	echo "Stopping process"
+	date
 fi
 
