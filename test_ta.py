75ab957ee1ae7d274c9db57b1e7f24152fab2bde
Dmitry <gdp@odev.io>
2021-09-30 15:40:49 +0300

    fix bug to connect

--- test_ta.py
+++ test_ta.py
@@ -15,12 +15,16 @@ def get_html_content(url: str) -> Optional[str]:
     s = requests.session()
 
     header = {
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-        "accept-encoding": "gzip, deflate, br",
-        "accept-language": "en-GB,en;q=0.9,en-US;q=0.8,ml;q=0.7",
-        "cache-control": "max-age=0",
-        "upgrade-insecure-requests": "1",
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36"
+        # "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        # "accept-encoding": "gzip, deflate, br",
+        # "accept-language": "en-GB,en;q=0.9,en-US;q=0.8,ml;q=0.7",
+        # "cache-control": "max-age=0",
+        # "upgrade-insecure-requests": "1",
+        # "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36"
+
+        "Connection": "Keep-Alive",
+        "Accept-Encoding": "identity",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36",
     }
 
     try:
