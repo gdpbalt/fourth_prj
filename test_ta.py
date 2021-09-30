56b2302680f1e3b0f51957bc00e10c84765b0fcf
Dmitry <gdp@odev.io>
2021-09-30 16:29:29 +0300

    fix bug to connect

--- test_ta.py
+++ test_ta.py
@@ -24,7 +24,7 @@ def get_html_content(url: str) -> Optional[str]:
 
         "Connection": "Keep-Alive",
         "Accept-Encoding": "identity",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36",
+        "User-Agent": "Wget/1.19.4 (linux-gnu)",
     }
 
     try:
