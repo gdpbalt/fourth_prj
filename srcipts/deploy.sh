bd97dfc9319aa7384b5f919bfe6386a4493f21dd
Dmitry <gdp@odev.io>
2021-10-13 16:50:13 +0300

    Add cryptography module

--- srcipts/deploy.sh
+++ srcipts/deploy.sh
@@ -6,17 +6,20 @@ cd /home/ubuntu/control/flask
 source /home/ubuntu/control/flask/venv/bin/activate
 export FLASK_APP=myapp.py
 
+# check current version
 /efs/var/www/control/flask/venv/bin/flask db current -v
 
 # login: gdpbalt
 # project: 
-/usr/bin/git pull
+/usr/bin/git fetch
+
+# install python modules
+/efs/var/www/control/flask/venv/bin/pip install -r requirements.txt
 
 # migrate
 /efs/var/www/control/flask/venv/bin/flask db upgrade
 
-# install python modules
-/efs/var/www/control/flask/venv/bin/pip install -r requirements.txt
+
 
 
 # test
