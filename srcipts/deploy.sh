4bf5d4710fd8fc98a9d080d2cbc8bd33bb2ede56
Dmitry <gdp@odev.io>
2021-10-13 17:30:47 +0300

    chg path to directory

--- srcipts/deploy.sh
+++ srcipts/deploy.sh
@@ -7,18 +7,18 @@ source /home/ubuntu/control/flask/venv/bin/activate
 export FLASK_APP=myapp.py
 
 # check current version
-/efs/var/www/control/flask/venv/bin/flask db current -v
+/home/ubuntu/control/flask/venv/bin/flask db current -v
 
 # login: gdpbalt
-# project: 
+# project: fourth_prj
 /usr/bin/git pull
 
 # install python modules
-/efs/var/www/control/flask/venv/bin/pip install -r requirements.txt
+/home/ubuntu/control/flask/venv/bin/pip install -r requirements.txt
 
 # migrate
-/efs/var/www/control/flask/venv/bin/flask db upgrade
+/home/ubuntu/control/flask/venv/bin/flask db upgrade
 
 # test
-/efs/var/www/control/flask/venv/bin/coverage run -m unittest discover
+/home/ubuntu/control/flask/venv/bin/coverage run -m unittest discover
 
