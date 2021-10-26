d7ceec2c9c1bb144835852b597a5a84eb9cf4bde
Dmitry <gdp@odev.io>
2021-10-26 18:32:09 +0300

    parse page for find full reviews

--- tmp/test_ta.py
+++ tmp/test_ta.py
@@ -1,52 +1,56 @@
-from typing import Optional
-import socket
-from urllib3.connection import HTTPConnection
+import json
+import re
 
 import requests
+from bs4 import BeautifulSoup
+
+# https://stackoverflow.com/questions/56682371/scraping-a-website-with-data-hidden-under-read-more
+HEADER = {
+    "Connection": "Keep-Alive",
+    "Accept-Encoding": "identity",
+    "User-Agent": "Wget/1.19.4 (linux-gnu)",
+}
+
+URL = "https://www.tripadvisor.ru/Hotel_Review-g303855-d1858675-Reviews-or5-Cleopatra_Luxury_Resort_Sharm_El_Sheikh-Nabq_Bay_Sharm_El_Sheikh_South_Sinai_Red_.html#REVIEWS"
+
+resp = requests.get(URL, headers=HEADER, timeout=30)
+bs = BeautifulSoup(resp.content, features="html.parser")
 
 
-def get_html_content(url: str) -> Optional[str]:
-    HTTPConnection.default_socket_options = HTTPConnection.default_socket_options + [
-        (socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1),
-        (socket.SOL_TCP, socket.TCP_KEEPIDLE, 45),
-        (socket.SOL_TCP, socket.TCP_KEEPINTVL, 10),
-        (socket.SOL_TCP, socket.TCP_KEEPCNT, 6)
-    ]
-    s = requests.session()
+tag = bs.find('script', text=re.compile('window.__WEB_CONTEXT__'))
+if tag is None:
+    print("Error parse input file")
+    exit(1)
 
-    header = {
-        # "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-        # "accept-encoding": "gzip, deflate, br",
-        # "accept-language": "en-GB,en;q=0.9,en-US;q=0.8,ml;q=0.7",
-        # "cache-control": "max-age=0",
-        # "upgrade-insecure-requests": "1",
-        # "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36"
+data = tag.text
+data = data.replace("window.__WEB_CONTEXT__=", "")
+data = data.replace('{pageManifest:', '{"pageManifest":')
+data = re.sub(r";\(.*$", "", data)
 
-        "Connection": "Keep-Alive",
-        "Accept-Encoding": "identity",
-        "User-Agent": "Wget/1.19.4 (linux-gnu)",
-    }
 
-    try:
-        print(f'GET {url}')
-        r = s.get(url, headers=header, verify=True, timeout=10)
-    except Exception as e:
-        msg = f'Network connection error'
-        print(f"{msg}. {e}")
+def get_reviews_from_page_manifest(input_data: dict):
+    if not isinstance(input_data, dict):
         return
 
-    print("Status: ", r.status_code)
-    if r.status_code != requests.codes.ok:
-        msg = f'Network connection error'
-        print(f'{msg}. Answer={r.status_code}')
+    result = input_data.get("pageManifest")
+    if not isinstance(result, dict):
         return
 
-    print("Get - OK")
-    return r.text
+    result = result.get("urqlCache")
+    if not isinstance(result, dict):
+        return
+
+    reviews = None
+    for review_key in result:
+        try:
+            reviews = result[review_key]['data']['locations'][0]['reviewListPage']['reviews']
+        except KeyError:
+            pass
+
+    if isinstance(reviews, list):
+        return reviews
 
 
-URL = "https://www.tripadvisor.ru/" + \
-      "Hotel_Review-g609052-d650787-Reviews-Kolibri_Resort_Hotel-Avsallar_Alanya_Turkish_Mediterranean_Coast.html "
-html = get_html_content(url=URL)
-print(html[:256])
+page_manifest = json.loads(data)
+page_reviews = get_reviews_from_page_manifest(page_manifest)
 print("Ok")
