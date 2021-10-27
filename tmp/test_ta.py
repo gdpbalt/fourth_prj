6199aaa1144fac36859138d1f2b65a6625108dc7
Dmitry <gdp@odev.io>
2021-10-27 12:31:41 +0300

    parse page for find full reviews

--- tmp/test_ta.py
+++ tmp/test_ta.py
@@ -1,10 +1,13 @@
 import json
 import re
+from pprint import pprint
+from typing import Optional
 
 import requests
 from bs4 import BeautifulSoup
 
 # https://stackoverflow.com/questions/56682371/scraping-a-website-with-data-hidden-under-read-more
+# https://control.antonivtours.com/api/review?hotelId=27766&access_token=298e9-774cd-1118a-5ccb7-fc8c6&page=2
 HEADER = {
     "Connection": "Keep-Alive",
     "Accept-Encoding": "identity",
@@ -28,7 +31,7 @@ data = data.replace('{pageManifest:', '{"pageManifest":')
 data = re.sub(r";\(.*$", "", data)
 
 
-def get_reviews_from_page_manifest(input_data: dict):
+def get_reviews_from_page_manifest(input_data: dict) -> Optional[list]:
     if not isinstance(input_data, dict):
         return
 
@@ -53,4 +56,26 @@ def get_reviews_from_page_manifest(input_data: dict):
 
 page_manifest = json.loads(data)
 page_reviews = get_reviews_from_page_manifest(page_manifest)
+output_list = list()
+for review in page_reviews:
+    output_dict = dict()
+    output_dict["index"] = review.get("id")
+    output_dict["published"] = review.get("publishedDate")
+    output_dict["rating"] = review.get("rating")
+    output_dict["title"] = review.get("title")
+    output_dict["text"] = review.get("text")
+
+    try:
+        output_dict["author"] = review["userProfile"]["displayName"]
+    except KeyError:
+        output_dict["author"] = None
+
+    try:
+        output_dict["avatar"] = review["userProfile"]["avatar"]["photoSizes"][1]["url"]
+    except KeyError:
+        output_dict["avatar"] = None
+
+    pprint(output_dict)
+    output_list.append(output_dict)
+
 print("Ok")
