ebf07257625ee982effe8ddee771ca10a1624a6a
Dmitry <gdp@odev.io>
2021-12-22 12:27:17 +0200

    move CORS_ORIGINS to env

--- config.py
+++ config.py
@@ -16,8 +16,7 @@ DATE_FORMAT = '%Y-%m-%d %H:%M'
 
 
 class Config:
-    CORS_ORIGINS = ["https://antonivtours.com", "https://dev.antonivtour.com", "https://antonivtour.com",
-                    "http://localhost:3001"]
+    CORS_ORIGINS = os.environ.get('CORS_ORIGINS') or 'http://localhost:3001'
 
     PROPAGATE_EXCEPTIONS = True
 
